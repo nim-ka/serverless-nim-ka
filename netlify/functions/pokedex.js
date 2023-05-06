1dd9fd8146f763d2f7dd9a48fa96f720b74a6975
nim-ka <alexing105@gmail.com>
2023-05-06 16:43:13 -0700

    Add multiple regions

--- netlify/functions/pokedex.js
+++ netlify/functions/pokedex.js
@@ -1,15 +1,18 @@
 // mod.cjs
-// eslint-disable-next-line no-shadow
+// eslint-disable-next-line no-shadow, import/no-extraneous-dependencies
 const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
-exports.handler = async () => {
-  const POKE_API = 'https://pokeapi.co/api/v2/pokedex/kanto';
+exports.handler = async (event) => {
+  const eventBody = JSON.parse(event.body);
+  const POKE_API = `https://pokeapi.co/api/v2/pokedex/${eventBody.region}`;
 
   const response = await fetch(POKE_API);
   const data = await response.json();
 
   return {
     statusCode: 200,
-    body: JSON.stringify(data),
+    body: JSON.stringify({
+      pokemon: data.pokemon_entries,
+    }),
   };
 };
